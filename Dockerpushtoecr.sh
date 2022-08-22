07c778363aa074d21db4d602253cb955fad84a72
root <root@ip-172-31-35-135.ap-south-1.compute.internal>
2022-08-22 16:02:05 +0000

    docker script

---
+++ Dockerpushtoecr.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 215767047920.dkr.ecr.ap-south-1.amazonaws.com
+sudo  docker tag dev 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:dev
+sudo  docker push 215767047920.dkr.ecr.ap-south-1.amazonaws.com/project/nodejs-app:dev
